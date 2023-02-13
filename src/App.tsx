2905b9d53db3d48fbd5ad84ef1d28f384022b05e
frisk038 <frisktrentehuit@gmail.com>
2023-02-13 22:39:00 +0100

    login page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import { useState } from "react";
+import { createBrowserRouter, Link, Outlet, RouterProvider } from "react-router-dom";
+import Header from "./components/header";
+import Login from "./pages/login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Header></Header>,
+    children: [
+      {
+        path: "/profile",
+        element: <div>Child Element!</div>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+interface HeaderProps {}
+
+const Header: FunctionComponent<HeaderProps> = () => {
+  return (
+    <>
+      <nav>
+        <div className="nav-wrapper">
+          <a href={`/`} className="brand-logo">
+            Hobby Chat
+          </a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li>
+              <Link to={`/profile/`}>Profile</Link>
+            </li>
+            <li>
+              <Link to={`/login/`}>Login</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Outlet />
+    </>
+  );
+};
+
+export default Header;
